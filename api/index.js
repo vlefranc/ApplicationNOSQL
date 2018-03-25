36497d6c32816d618f8aaec51be4578c7d29c8d5
unknown <violette.Lefranc@devinci.fr>
2018-03-25 23:16:46 +0200

    index

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const Company = require('./model');
+const CompanyRouter = require('express').Router;
+const CompanyQuery = require('.queries');
+
+moviesRouter.get('/competitors', (req, res ,next) => {
+    MoviesController.sortCompetitors(req, req, (err,list) => {
+          if(err) {
+                console.log("Error " + err);
+                res.sendStatus(500);
+          } else {
+                res.json(list);
+          }
+    });
+});
+
+moviesRouter.get('/buyers', (req, res ,next) => {
+    MoviesController.GroupByBuyers(req, req, (err,list) => {
+          if(err) {
+                console.log("Error " + err);
+                res.sendStatus(500);
+          } else {
+                res.json(list);
+          }
+    });
+});
+
+moviesRouter.get('/offices', (req, res ,next) => {
+    MoviesController.PrintOfficesEuro(req, req, (err,list) => {
+          if(err) {
+                console.log("Error " + err);
+                res.sendStatus(500);
+          } else {
+                res.json(list);
+          }
+    });
+});
---
+++ api/queries.js
@@ -0,0 +1,48 @@
+const Company = require('./model');
+
+//Company.find({},{"name":1,"_id":0,"homepage_url":1, "overview":1,})
+
+
+//Company.find({"total_money_raised":{ $regex: "€", $options: 'si' }},{"name":1,"_id":0,"total_money_raised":1})
+
+//Sort the companies in descending order by the number of their competitors. 
+//(In order to buy the companies with less competitors to crush them.)
+
+exports.sortCompetitors = function(callback){
+
+    var matchEmployees={$match:{"competitions":{'$not': {'$size': 0}}}}
+    var unwindCompetitors={$unwind: "$competitions"}
+    var groupComp={$group : {"_id":"$name","nb_of_competitors":{$sum:1}}}
+    var sortDown={$sort:{"nb_of_competitors":1}}
+    Company.aggregate([unwindCompetitors,matchEmployees,groupComp,sortDown], callback)
+
+};
+
+//For each company acquired by another one, group them by the name of the buying 
+//company and sort these buyers by the number of companies they bought.
+
+exports.GroupByBuyers = function(callback){
+    var unwindCompany ={$unwind:"$acquisition.acquiring_company.name"};
+var groupByBuyers ={$group: {"_id":"$acquisition.acquiring_company.name","nb_of_companies_bought":{$sum:1}}};
+var sortByNumberBought= {$sort:{"nb_of_companies_bought":-1}};
+Company.aggregate([unwindCompany, groupByBuyers, sortByNumberBought], callback)
+
+};
+
+
+//For each company, print their name and the addresses of their offices in the US, 
+//only if the total money they raised is in Euro.
+
+exports.PrintOfficesEuro = function(callback)
+{
+
+    var unwindOffice= {$unwind : "$offices"}
+var matchEuroInUsa={$match:{"offices.country_code":"USA","total_money_raised":{$regex:"€",$options:"i"}}}
+var projectInfo= {$project: {"_id":0,"name":1}}
+var groupBy={$group: {"_id":"$name","nb_of_offices":{$sum:1}}}
+var sortByAsc={$sort:{"nb_of_offices":-1}}
+Company.aggregate([unwindOffice, matchEuroInUsa,projectInfo,groupBy,sortByAsc], callback);
+};
+
+
+
